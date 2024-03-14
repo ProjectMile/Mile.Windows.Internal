106f6b5eddb24700fb7e38a1cd5e396b78b3f02f
MouriNaruto <Kenji.Mouri@outlook.com>
2024-03-14 20:51:27 +0800

    Add Mile.Internal.RefreshPackageVersion project.

---
+++ Mile.Internal.RefreshPackageVersion/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Mile.Internal.RefreshPackageVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "Mile.Internal.RefreshPackageVersion task has been completed.");
+            Console.ReadKey();
+        }
+    }
+}
